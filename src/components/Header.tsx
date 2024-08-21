6adf1719b21e5434dba38e21b6d696c74387b523
박지혜 <jihyeee0112@naver.com>
2024-08-21 21:10:43 +0900

    .env 파일 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,27 +4,31 @@ import Logo from '../images/Logo.svg';
 
 const StyledHeader = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  margin-top: 1.5vh;
+  margin-left: 1.5vh;
 `;
 
 const StyledLogo = styled.img`
+  width: 15vw;
 
-@media (min-width: 480px) {
-    width: 30%;
-}
+  max-width: 150px;
+  min-width: 100px;
 
-@media (min-width: 600px) {
-    width: 20%;
-}
+  @media (min-width: 480px) {
+    width: 14vw;
+  }
 
-@media (min-width: 1024px) {
-    width: 15%;
-}
+  @media (min-width: 600px) {
+    width: 12vw;
+  }
 
-@media (min-width: 1280px) {
-    width: 10%;
-}
+  @media (min-width: 1024px) {
+    width: 10vw;
+  }
+
+  @media (min-width: 1280px) {
+    width: 8vw;
+  }
 `;
 
 const Header: React.FC = () => {
